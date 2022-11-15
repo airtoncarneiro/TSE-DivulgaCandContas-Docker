4251ae291b1b065929aa38e99c17c1d39139354e
Airton Carneiro <airtoncarneiro@gmail.com>
2022-11-15 08:59:34 -0300

    Incluir excecao ao ler JSON vazio

--- lib/tse_candidatos.py
+++ lib/tse_candidatos.py
@@ -3,6 +3,7 @@ import os
 import re
 from configparser import ConfigParser, ExtendedInterpolation
 from pathlib import Path
+import logging
 
 import urllib3
 
@@ -51,9 +52,11 @@ class TSE_Candidatos():
                 " (KHTML, like Gecko) Chrome/15.0.861.0 Safari/535.2"
         }
         if input_folder:
-            self.input_folder = os.path.join(os.getcwd(), os.path.normpath(input_folder))
+            self.input_folder = os.path.join(os.getcwd(), 
+                                             os.path.normpath(input_folder))
             self.input_file = input_file
-        self.output = os.path.join(os.getcwd(), output_folder, os.path.normpath(output_file))
+        self.output = os.path.join(os.getcwd(), output_folder, 
+                                   os.path.normpath(output_file))
 
     def save(self, full_file_name:str)->None:
         """Salva o arquivo.
@@ -63,6 +66,7 @@ class TSE_Candidatos():
         """
         os.makedirs(os.path.dirname(full_file_name), exist_ok=True)
         with open(full_file_name, "w", encoding="utf8") as file:
+            logging.info("Output File: {}".format(full_file_name))
             json.dump(self.r, file, ensure_ascii=False)
     
     def read_from_file(self)->dict:
@@ -76,28 +80,38 @@ class TSE_Candidatos():
                 pode ter uma lista de dicionários, retornar cada um com Yield.
         """
         files = Path(self.input_folder).rglob(self.input_file)
+        if (sum(1 for _ in files)) == 0:
+            logging.info("Não há arquivos '{}'!".format(self.input_file))
+        else:
+            files = Path(self.input_folder).rglob(self.input_file)
+
         for file in files:
             custom_dict = self._makeADictFromPath(str(file))
             with open(file, encoding="utf-8") as json_file:
-                objs = json.load(json_file)
-                # Como a iteração deve ser na lista vindo do JSON
-                # e, algumas vezes, a lista não é o único objeto
-                # do arquivo, garantir retornar sempre uma lista
-                if type(objs) == list:
-                    for obj in objs:
-                        for key, value in custom_dict.items():
-                            obj[key] = value
-                        yield obj
-                else:
-                    for _, value in objs.items():
-                        if type(value) == list:
-                            for obj in value:
-                                if "candidatos" in objs and \
-                                not("codCand") in obj:
-                                   obj["codCand"] = obj["id"]
-                                for key, value in custom_dict.items():
-                                    obj[key] = value
-                                yield obj
+                try:
+                    objs = json.load(json_file)
+                    # Como a iteração deve ser na lista vindo do JSON
+                    # e, algumas vezes, a lista não é o único objeto
+                    # do arquivo, garantir retornar sempre uma lista
+                    if type(objs) == list:
+                        for obj in objs:
+                            for key, value in custom_dict.items():
+                                obj[key] = value
+                            yield obj
+                    else:
+                        for _, value in objs.items():
+                            if type(value) == list:
+                                for obj in value:
+                                    if "candidatos" in objs and \
+                                    not("codCand") in obj:
+                                        obj["codCand"] = obj["id"]
+                                    for key, value in custom_dict.items():
+                                        obj[key] = value
+                                    yield obj
+                except ValueError:
+                    logging.warning("Erro ao ler o arquivo '{}'!"\
+                        .format(json_file.name))
+        
     
     def download(self, url:str, pool_manager:urllib3.PoolManager)->dict:
         """Realiza o método GET na API do TSE.
@@ -138,7 +152,8 @@ class TSE_Candidatos():
         # Substitui os parâmetros da URL por seus respectivos valores
         for matched in re.findall(pattern, custom_text):
             key = [matched[1:-1]][0]
-            custom_text = custom_text.replace(matched, str(custom_dict.get(key, matched)))
+            custom_text = custom_text.\
+                replace(matched, str(custom_dict.get(key, matched)))
 
         if not 'http' in custom_text:
             os.path.normpath(custom_text)
@@ -157,8 +172,9 @@ class TSE_Candidatos():
             dict: Path com as chaves/valores substituídos.
         """
         customDict = dict()
-        for key_value in path.split("\\"):
-            if "=" in key_value:
-                key, value = key_value.split("=")
-                customDict[key] = value
+        pattern = "\w*=\w*"
+        # Substitui os parâmetros da URL por seus respectivos valores
+        for key_value in re.findall(pattern, path):
+            key, value = key_value.split("=")
+            customDict[key] = value
         return customDict
